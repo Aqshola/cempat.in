22acb437db3319fd3d45bb34b52bbd8d56c7c74d
aqshol afid <aqsholafid2@gmail.com>
2022-02-27 14:15:08 +0700

    feat: add wyiwysg editor and create tulis cerita section

---
+++ src/helper/useOutsideClick.ts
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+
+export default function useOutsideClick(ref: any, callback: () => void) {
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    }
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref, callback]);
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins: ["Poppins", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
