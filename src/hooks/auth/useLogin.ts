41ae87a5881ab03e4a98f578b18f4d1672569e41
aqshol afid <aqsholafid2@gmail.com>
2022-03-28 20:37:30 +0700

    feat: add hooks for auth

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,19 +1,24 @@
 import supabase from "lib/Supabase";
 import React, { useState } from "react";
-import { User, ApiError } from "@supabase/supabase-js";
+import { User, ApiError, PostgrestError } from "@supabase/supabase-js";
 
+type UserData = {
+  user_id: string;
+  email: string;
+  username: string;
+};
 export function useLogin(): [
   (email: string, password: string) => void,
   {
-    user: User | null;
-    error: ApiError | null;
+    user: UserData | null;
+    error: ApiError | null | PostgrestError;
   },
   boolean
 ] {
   const [loading, setloading] = useState<boolean>(false);
   const [data, setdata] = useState<{
-    user: User | null;
-    error: ApiError | null;
+    user: UserData | null;
+    error: ApiError | null | PostgrestError;
   }>({
     user: null,
     error: null,
@@ -26,7 +31,33 @@ export function useLogin(): [
         email,
         password,
       });
-      setdata({ user, error });
+
+      if (error) {
+        setdata({ user: null, error });
+      }
+
+      if (user) {
+        const { data, error: ErrorSelect } = await supabase
+          .from("user")
+          .select("username, id, user_id")
+          .eq("user_id", user.id);
+        if (data) {
+          setdata({
+            user: {
+              user_id: data[0].user_id,
+              username: data[0].username,
+              email: user.email || "",
+            },
+            error: null,
+          });
+        } else {
+          setdata({
+            user: null,
+            error: ErrorSelect,
+          });
+        }
+      }
+
       setloading(false);
     },
     data,
